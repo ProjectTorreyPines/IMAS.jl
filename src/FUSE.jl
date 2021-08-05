c0164ed54920788b4acf21ee0bea43742037d0bf
Orso Meneghini <orso82@gmail.com>
2021-08-05 14:19:36 -0700

    Demonstrate ability to resize and deepcopy parts of the data structure

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -4,12 +4,15 @@ module FUSE
 
 include("dd.jl")
 
+import StructArrays
+import Base: resize!
+
 """
 Resize StructArray to contain n elements.
 If n is smaller than the current collection length, the first n elements will be retained.
 If n is larger, the new elements are not guaranteed to be initialized.
 """
-function resize!(collection::StructArray, n::Int)
+function Base.resize!(collection::StructArrays.StructArray, n::Int)
     if n > length(collection)
         for k in length(collection):n - 1
             push!(collection, eltype(collection)())
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,20 +10,25 @@ using Test
     # data.core_profiles.profiles_1d[1].electrons.density = (1.0 .- data.core_profiles.profiles_1d[1].grid.rho_tor_norm).^2
     # @test length(data.core_profiles.profiles_1d[1].grid.rho_tor_norm) == 10
     
-    cp = FUSE.core_profiles()
-    cp.profiles_1d[1].grid.rho_tor_norm = range(0, 1, length=10)
-    cp.profiles_1d[1].electrons.density = (1.0 .- cp.profiles_1d[1].grid.rho_tor_norm).^2
-    @test length(cp.profiles_1d[1].grid.rho_tor_norm) == 10
+    crp = FUSE.core_profiles()
+    crp.profiles_1d[1].grid.rho_tor_norm = range(0, 1, length=10)
+    crp.profiles_1d[1].electrons.density = (1.0 .- crp.profiles_1d[1].grid.rho_tor_norm).^2
+    @test length(crp.profiles_1d[1].grid.rho_tor_norm) == 10
 
     # resize an array of struct
-    resize!(cp.profiles_1d, 2)
-    @test length(cp.profiles_1d) == 2
-    cp.profiles_1d[1]=cp.profiles_1d[2]
+    resize!(crp.profiles_1d, 2)
+    @test length(crp.profiles_1d) == 2
+
+    # deepcopy of one arrray structure element to another
+    crp.profiles_1d[2] = deepcopy(crp.profiles_1d[1])
+    crp.profiles_1d[2].electrons.density .*= 2.0
+    @test all(crp.profiles_1d[2].grid.rho_tor_norm .== crp.profiles_1d[1].grid.rho_tor_norm)
+    @test all(crp.profiles_1d[2].electrons.density .== (crp.profiles_1d[1].electrons.density * 2.0))
 
     # working with data that is not time dependent? --> only use the relevant struct
-    cp1d = FUSE.core_profiles__profiles_1d()
-    cp1d.grid.rho_tor_norm = range(0, 1, length=10)
-    cp1d.electrons.density = (1.0 .- cp1d.grid.rho_tor_norm).^2
-    @test length(cp1d.grid.rho_tor_norm) == 10
+    crp1d = FUSE.core_profiles__profiles_1d()
+    crp1d.grid.rho_tor_norm = range(0, 1, length=10)
+    crp1d.electrons.density = (1.0 .- crp1d.grid.rho_tor_norm).^2
+    @test length(crp1d.grid.rho_tor_norm) == 10
 
 end
