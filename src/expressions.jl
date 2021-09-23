28a7682c5491296acc113a94d5687d636bff4751
Orso Meneghini <orso82@gmail.com>
2021-09-22 23:40:22 -0700

    more expressions

--- src/expressions.jl
+++ src/expressions.jl
@@ -20,13 +20,21 @@ expressions["equilibrium.time_slice[:].global_quantities.energy_mhd"] =
     (;time_slice, _...) -> 3 / 2 * trapz(time_slice.profiles_1d.volume, time_slice.profiles_1d.pressure)
 
 
+expressions["equilibrium.time_slice[:].global_quantities.q_95"] =
+    (;time_slice, _...) -> Interpolations.LinearInterpolation(time_slice.profiles_1d.rho_tor_norm,time_slice.profiles_1d.q)(0.95)
+
+expressions["equilibrium.time_slice[:].global_quantities.q_axis"] =
+    (;time_slice, _...) -> time_slice.profiles_1d.q[1]
+
+expressions["equilibrium.time_slice[:].global_quantities.q_min"] =
+    (;time_slice, _...) -> minimum(time_slice.profiles_1d.q)
+
+
 expressions["equilibrium.time_slice[:].boundary.geometric_axis"] =
     (;time_slice, _...) -> (time_slice.profiles_1d.r_outboard[end] + time_slice.profiles_1d.r_inboard[end]) * 0.5
 
-    
 expressions["equilibrium.time_slice[:].boundary.minor_radius"] =
     (;time_slice, _...) -> (time_slice.profiles_1d.r_outboard[end] - time_slice.profiles_1d.r_inboard[end]) * 0.5
-
 
 expressions["equilibrium.time_slice[:].boundary.elongation"] =
     (;time_slice, _...) -> (time_slice.boundary.elongation_lower + time_slice.boundary.elongation_upper) * 0.5
