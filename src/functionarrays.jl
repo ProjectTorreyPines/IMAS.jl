2d6752adcc8d4fdf236f37397e8022bdd76cee2e
Orso Meneghini <orso82@gmail.com>
2021-10-03 21:03:27 -0700

    better error handling

--- src/functionarrays.jl
+++ src/functionarrays.jl
@@ -86,7 +86,12 @@ function Base.setproperty!(ids::IDS, field::Symbol, v; skip_non_coordinates=fals
             end
         end
     end
-    setfield!(ids, field, v)
+    try
+        setfield!(ids, field, v)
+    catch e
+        typeof(e) <: TypeError && error("$(typeof(v)) is the wrong type for $(f2u(ids)).$(field), it should be $(typeof(getfield(ids, field)))")
+        rethrow()
+    end
 end
 
 """
