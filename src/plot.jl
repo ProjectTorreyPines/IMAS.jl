493f54c95b44e27d6fb39790804fef5624a6faa3
Orso Meneghini <orso82@gmail.com>
2021-11-06 07:33:30 -0700

    Improve plotting of equilibrium and radial_build

--- src/plot.jl
+++ src/plot.jl
@@ -21,17 +21,24 @@ end
 
 Plots equilibrium cross-section
 """
-@recipe function plot_eqcx(eqt::IMAS.equilibrium__time_slice; lcfs=false)
+@recipe function plot_eqcx(eqt::IMAS.equilibrium__time_slice; psi_levels=nothing, psi_levels_out=nothing, lcfs=false)
     if lcfs
         psi_levels = [eqt.global_quantities.psi_boundary, eqt.global_quantities.psi_boundary]
         psi_levels_out = []
-        
     else
-        psi_levels = range(eqt.global_quantities.psi_axis, eqt.global_quantities.psi_boundary, length=11)
-        psi_levels_out = (psi_levels[end] - psi_levels[1]) .* collect(range(0, 1, length=11)) .+ psi_levels[end]
+        if psi_levels === nothing
+            psi_levels = range(eqt.global_quantities.psi_axis, eqt.global_quantities.psi_boundary, length=11)
+        elseif isa(psi_levels, Int)
+            psi_levels = range(eqt.global_quantities.psi_axis, eqt.global_quantities.psi_boundary, length=psi_levels)
+        end
+        if psi_levels_out === nothing
+            psi_levels_out = (psi_levels[end] - psi_levels[1]) .* collect(range(0, 1, length=11)) .+ psi_levels[end]
+        elseif isa(psi_levels_out, Int)
+            psi_levels_out = (psi_levels[end] - psi_levels[1]) .* collect(range(0, 1, length=psi_levels_out)) .+ psi_levels[end]
+        end
     end
 
-    seriestype --> :path
+
     label --> ""
     aspect_ratio --> :equal
     primary --> false
@@ -39,9 +46,15 @@ Plots equilibrium cross-section
     xlims --> eqt.profiles_2d[1].grid.dim1[1], eqt.profiles_2d[1].grid.dim1[end]
     ylims --> eqt.profiles_2d[1].grid.dim2[1], eqt.profiles_2d[1].grid.dim2[end]
 
+    # @series begin
+    #     seriestype --> :contour
+    #     levels --> vcat(psi_levels[2:end], psi_levels_out)
+    #     eqt.profiles_2d[1].grid.dim1, eqt.profiles_2d[1].grid.dim2, transpose(eqt.profiles_2d[1].psi)
+    # end
     for psi_level in vcat(psi_levels[2:end], psi_levels_out)
         for (pr, pz) in IMAS.flux_surface(eqt, psi_level, false)
             @series begin
+                seriestype --> :path
                 if psi_level == eqt.global_quantities.psi_boundary
                     linewidth --> 2
                 else
@@ -64,11 +77,13 @@ end
 
 Plots radial build cross-section
 """
+
 @recipe function plot_radial_build_cx(rb::IMAS.radial_build)
-    seriestype --> :vline
     aspect_ratio --> :equal
-    linewidth --> 2
+    grid --> :none
     @series begin
+        seriestype --> :vline
+        linewidth --> 2
         label --> "center"
         linestyle --> :dash
         color --> :black
@@ -77,9 +92,37 @@ Plots radial build cross-section
     at = 0
     for l in rb.center_stack
         @series begin
-            at += l.thickness
+            if ! is_missing(l,:material) && l.material=="vacuum"
+                color --> :white
+            end
+            if occursin("OH",l.name)
+                color --> :blue
+            end
+            if occursin("TF",l.name)
+                color --> :green
+            end
+            if occursin("shield",l.name)
+                color --> :red
+            end
+            if occursin("blanket",l.name)
+                color --> :orange
+            end
+            if occursin("wall",l.name)
+                color --> :yellow
+            end
+            seriestype --> :vspan
             label --> l.name
-            xlim --> [0,at*1.5]
+            alpha --> 0.2
+            xlim --> [0,at*2.0]
+            [at,at+l.thickness]
+        end
+        at += l.thickness
+        @series begin
+            seriestype --> :vline
+            linewidth --> 0.5
+            label --> ""
+            color --> :black
+            xlim --> [0,at*2.0]
             [at]
         end
     end
