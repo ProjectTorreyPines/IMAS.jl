8495802a2b0c9d8c2cf92dd46b58f61087403e01
Orso Meneghini <orso82@gmail.com>
2021-12-07 11:50:05 -0800

    OMAS_ROOT as environment variable

--- src/generate_dd.jl
+++ src/generate_dd.jl
@@ -5,7 +5,9 @@ using ProgressMeter
 ProgressMeter.ijulia_behavior(:clear)
 
 const imas_version = "3_33_0"
-ENV["OMAS_ROOT"] = "/Users/meneghini/Coding/atom/omas"
+if ! ("OMAS_ROOT" in keys(ENV))
+    error("Environmental variable OMAS_ROOT is not set")
+end
 const omas_imas_structure_folder = joinpath(ENV["OMAS_ROOT"], "omas", "imas_structures")
 run(`sh -c "rm -rf $(dirname(dirname(@__FILE__)))/data_structures"`)
 run(`sh -c "mkdir $(dirname(dirname(@__FILE__)))/data_structures"`)
