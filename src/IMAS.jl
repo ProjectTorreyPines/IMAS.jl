a1af063142bfe23b86d8f9c630cd3b6382347fc5
Orso Meneghini <orso82@gmail.com>
2022-01-26 06:29:10 -0800

    export only @timedep and is_missing

--- src/IMAS.jl
+++ src/IMAS.jl
@@ -28,11 +28,6 @@ include("plot.jl")
 #= ====== =#
 #= EXPORT =#
 #= ====== =#
-export f2p, f2u, p2i, i2p, u2f, f2f, f2fs, u2fs, f2i
-export coordinates, set_timedep_value!
-export IDS, IDSvector, IDSvectorElement
-export top, top_dd, top_ids, parent, children, expressions
-
 export is_missing, timedep
 
 end # module
