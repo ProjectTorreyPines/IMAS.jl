f8cc2f4e455d234c8ba78dc43372f72a09a324e7
Orso Meneghini <orso82@gmail.com>
2022-01-27 13:24:01 -0800

    Cleanup unecessary set_timedep_value! function

--- src/time.jl
+++ src/time.jl
@@ -1,48 +1,12 @@
 """
-    set_timedep_value!(time_ids::IDS, value_ids::IDS, value_symbol::Symbol, time0::Real, value0::Real; time_symbol::Symbol=:time)
-
-Set the value of a time-depentent quantity
-"""
-function set_timedep_value!(time_ids::IDS, value_ids::IDS, value_symbol::Symbol, time0::Real, value0::Real; time_symbol::Symbol = :time)
-    if is_missing(value_ids, value_symbol) || is_missing(time_ids, time_symbol)
-        setproperty!(time_ids, time_symbol, [time0])
-        setproperty!(value_ids, value_symbol, [value0])
-    else
-        time = getproperty(time_ids, time_symbol)
-        value = getproperty(value_ids, value_symbol)
-        if any(time .== time0)
-            time_index = argmin(abs.(time .- time0))
-            value[time_index] = value0
-        elseif time0 > maximum(time)
-            push!(time, time0)
-            push!(value, value0)
-        elseif time0 < minimum(time)
-            pushfirst!(time, time0)
-            pushfirst!(value, value0)
-        else
-            for (k, t) in enumerate(time)
-                if t < time0
-                    continue
-                else
-                    setproperty!(time_ids, time_symbol, vcat(time[1:k-1], time0, time[k:end]))
-                    setproperty!(value_ids, value_symbol, vcat(value[1:k-1], value0, value[k:end]))
-                    break
-                end
-            end
-        end
-    end
-    return getproperty(time_ids, time_symbol), getproperty(value_ids, value_symbol)
-end
-
-"""
     look for time array information
 """
-function time_array(x::Union{IDS,IDSvector{T}}) where {T<:IDSvectorElement}
+function time_array(ids::Union{IDS,IDSvector{T}}) where {T<:IDSvectorElement}
     time_array = nothing
     missing_time_locations = []
 
     # traverse IDS hierarchy upstream looking for a time array
-    h = x
+    h = ids
     while h._parent.value !== missing
         field_names_types = NamedTuple{fieldnames(typeof(h))}(fieldtypes(typeof(h)))
         if :time in keys(field_names_types) && typeintersect(field_names_types[:time], AbstractVector) !== Union{}
@@ -59,7 +23,7 @@ function time_array(x::Union{IDS,IDSvector{T}}) where {T<:IDSvectorElement}
     end
     if time_array === nothing
         if length(missing_time_locations) == 0
-            error("Could not find time array information for $(p2i(f2p(x)[1:end-1]))[$time]")
+            error("Could not find time array information for $(p2i(f2p(ids)[1:end-1]))[$time]")
         else
             time_array = missing_time_locations[end].time = Float64[]
         end
@@ -80,7 +44,7 @@ end
 
 set data to a time-dependent array at the dd.global_time
 """
-function set_time_array(ids, location, value)
+function set_time_array(ids::Union{IDS,IDSvector{T}}, location::Symbol, value) where {T<:IDSvectorElement}
     time = time_array(ids)
     time0 = global_time(ids)
     # no time information
@@ -131,7 +95,7 @@ end
 
 get data from a time-dependent array at the dd.global_time
 """
-function get_time_array(ids, location)
+function get_time_array(ids::Union{IDS,IDSvector{T}}, location::Symbol) where {T<:IDSvectorElement}
     time = time_array(ids)
     time0 = global_time(ids)
     i = argmin(abs.(time .- time0))
