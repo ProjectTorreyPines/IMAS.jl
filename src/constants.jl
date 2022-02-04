d29674fc2c7ef491e213eacc562971f385008ca3
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2022-02-04 13:49:25 -0800

    Update src/constants.jl
    Co-authored-by: Orso Meneghini <orso82@gmail.com>

--- src/constants.jl
+++ src/constants.jl
@@ -1,27 +1,16 @@
 import PhysicalConstants.CODATA2018 as PCs
 
-struct PhysicsConstants
-    μ_0 :: Float64
-    c_0 :: Float64
-    ϵ_0 :: Float64
-    k_B :: Float64
-    e :: Float64
-    m_e :: Float64
-    m_p :: Float64
-    m_n :: Float64
-    atm :: Float64
-    m_u :: Float64
+@kwdef struct PhysicsConstants
+    μ_0 :: Float64 = float(Float64, PCs.μ_0).val
+    c_0 :: Float64 = float(Float64, PCs.c_0).val
+    ϵ_0 :: Float64 = float(Float64, PCs.ε_0).val
+    k_B :: Float64 = float(Float64, PCs.k_B).val
+    e :: Float64 = float(Float64, PCs.e).val
+    m_e :: Float64 =  float(Float64, PCs.m_e).val
+    m_p :: Float64 =  float(Float64, PCs.m_p).val
+    m_n :: Float64 =  float(Float64, PCs.m_n).val
+    atm :: Float64 = float(Float64, PCs.atm).val
+    m_u :: Float64 =  float(Float64, PCs.m_u).val
 end
 
-Constants = PhysicsConstants(
-    float(Float64, PCs.μ_0).val,
-    float(Float64, PCs.c_0).val,
-    float(Float64, PCs.ε_0).val,
-    float(Float64, PCs.k_B).val,
-    float(Float64, PCs.e).val,
-    float(Float64, PCs.m_e).val,
-    float(Float64, PCs.m_p).val,
-    float(Float64, PCs.m_n).val,
-    float(Float64, PCs.atm).val,
-    float(Float64, PCs.m_u).val
-    )
+Constants = PhysicsConstants()
