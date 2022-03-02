ef15e6754dfa41b30dfca10dc69e07a2e633eb7a
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2022-03-02 09:16:05 -0800

    add Sauter bootstrap neo 2021 correction

--- src/physics.jl
+++ src/physics.jl
@@ -838,6 +838,10 @@ function lnLambda_e(ne, Te)
     return 23.5 .- log.(sqrt.(ne ./ 1e6) .* Te .^ (-5.0 ./ 4.0)) .- (1e-5 .+ (log.(Te) .- 2) .^ 2 ./ 16.0) .^ 0.5
 end
 
+function lnLambda_i(ni, Ti, Zavg)
+    return 30.0 .- log.(Zavg .^ 3 * sqrt.(ni) ./ (Ti .^ 1.5))
+end
+
 function spitzer_conductivity(ne, Te, Zeff)
     return 1.9012e4 .* Te .^ 1.5 ./ (Zeff .* 0.58 .+ 0.74 ./ (0.76 .+ Zeff) .* lnLambda_e(ne, Te))
 end
@@ -863,6 +867,24 @@ function nuestar(dd::IMAS.dd)
     return 6.921e-18 .* abs.(q) .* R .* ne .* Zeff .* lnLambda_e(ne, Te) ./ (Te .^ 2 .* eps .^ 1.5)
 end
 
+function nuistar(dd::IMAS.dd)
+    eqt = dd.equilibrium.time_slice[]
+    cp1d = dd.core_profiles.profiles_1d[]
+
+    rho = cp1d.grid.rho_tor_norm
+    Zeff = cp1d.zeff
+
+    R = (eqt.profiles_1d.r_outboard + eqt.profiles_1d.r_inboard) / 2.0
+    R = IMAS.interp(eqt.profiles_1d.rho_tor_norm, R)(rho)
+    a = (eqt.profiles_1d.r_outboard - eqt.profiles_1d.r_inboard) / 2.0
+    a = IMAS.interp(eqt.profiles_1d.rho_tor_norm, a)(rho)
+
+    eps = a ./ R
+
+    q = IMAS.interp(eqt.profiles_1d.rho_tor_norm, eqt.profiles_1d.q)(rho)
+
+    return 4.90e-18 * abs.(q) .* R .* ni * Zeff .^ 4 * lnLambda_i(ni, Ti, Zeff) / (Ti .^ 2 .* eps .^ 1.5)
+end
 
 function collision_frequencies(dd::IMAS.dd)
     # from TGYRO `collision_rates` subroutine
@@ -875,7 +897,7 @@ function collision_frequencies(dd::IMAS.dd)
     e = 4.8032e-10 # statcoul
     k = 1.6022e-12 # erg/eV
 
-    loglam = 24.0.-log.(sqrt.(ne)./Te)
+    loglam = 24.0 .- log.(sqrt.(ne) ./ Te)
 
     # 1/tau_ee (Belli 2008) in 1/s
     nue = sqrt(2) .* pi .* ne * e^4.0 .* loglam ./ (sqrt(me) * (k * Te) .^ 1.5)
@@ -904,6 +926,94 @@ function collision_frequencies(dd::IMAS.dd)
     end
 
     return nue, nui, nu_exch
+end
+
+function Sauter_neo2021_bootsrap(dd::IMAS.dd)
+
+    eqt = dd.equilibrium.time_slice[]
+    cp1d = dd.core_profiles.profiles_1d[]
+
+    rho = cp1d.grid.rho_tor_norm
+    Te = cp1d.electrons.temperature
+
+    pressure_thermal = cp1d.pressure_thermal
+    Zeff = cp1d.zeff
+
+    R_pe = cp1d.electrons.pressure ./ pressure_thermal
+
+    fT = IMAS.interp(eqt.profiles_1d.rho_tor_norm, eqt.profiles_1d.trapped_fraction)(rho)
+
+    nue = nuestar(dd)
+    nui = nuistar(dd)
+
+    # neo 2021
+    f31teff = fT ./ (
+        1
+        .+
+        (0.67 .* (1 .- 0.7 .* fT) .* sqrt.(nue)) ./ (0.56 .+ 0.44 .* Zeff)
+        .+
+        (0.52 .+ 0.086 .* sqrt.(nue)) .* (1 .+ 0.87 .* fT) .* nue ./ (1 .+ 1.13 .* (Zeff .- 1) .^ 0.5))
+    X = f31teff
+    F31 = (
+        (1 .+ 0.15 ./ (Zeff .^ 1.2 .- 0.71)) .* X
+        .-
+        0.22 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 2
+        .+
+        0.01 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 3
+        .+
+        0.06 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 4
+    )
+
+
+    X = f32eeteff
+    F32ee = (0.1 .+ 0.6 .* Zeff) ./ (Zeff .* (0.77 .+ 0.63 .* (1 .+ (Zeff .- 1) .^ 1.1))) .* (X .- X .^ 4)
+    (
+        .+0.7 ./ (1 .+ 0.2 .* Zeff) .* (X .^ 2 .- X .^ 4 .- 1.2 .* (X .^ 3 .- X .^ 4)) .+ 1.3 ./ (1 .+ 0.5 .* Zeff) .* X .^ 4
+    )
+
+    f32eiteff = fT ./ (
+        1
+        .+
+        ((0.87 .* (1 .+ 0.39 .* fT) .* sqrt.(nue)) ./ (1 .+ 2.95 .* (Zeff .- 1) .^ 2))
+        .+
+        1.53 .* (1 .- 0.37 .* fT) .* nue .* (2 .+ 0.375 .* (Zeff .- 1)))
+
+    Y = f32eiteff
+
+    F32ei = (
+        .-(0.4 .+ 1.93 .* Zeff) ./ (Zeff .* (0.8 .+ 0.6 .* Zeff)) .* (Y .- Y .^ 4)
+        .+
+        5.5 ./ (1.5 .+ 2 .* Zeff) .* (Y .^ 2 .- Y .^ 4 .- 0.8 .* (Y .^ 3 .- Y .^ 4))
+        .-
+        1.3 ./ (1 .+ 0.5 .* Zeff) .* Y .^ 4
+    )
+
+    L_32 = F32ee .+ F32ei
+
+    X = f34teff
+    L_34 = (
+        (1 .+ 0.15 ./ (Zeff .^ 1.2 .- 0.71)) .* X
+        .-
+        0.22 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 2
+        .+
+        0.01 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 3
+        .+
+        0.06 ./ (Zeff .^ 1.2 .- 0.71) .* X .^ 4
+    )
+    alpha0 = (
+        .-(0.62 .+ 0.055 .* (Zeff .- 1)) ./ (0.53 .+ 0.17 .* (Zeff .- 1)) .* (1 .- fT) ./ (1 .- (0.31 .- 0.065 .* (Zeff .- 1)) .* fT .- 0.25 .* fT .^ 2)
+    )
+    alpha = ((alpha0 .+ 0.7 .* Zeff .* fT .^ 0.5 .* sqrt.(nui)) ./ (1 .+ 0.18 .* sqrt.(nui)) .- 0.002 .* nui .^ 2 .* fT .^ 6) .* (
+        1 ./ (1 .+ 0.004 .* nui .^ 2 .* fT .^ 6)
+    )
+
+    bra1 = F31 .* IMAS.gradient(pressure_thermal, rho) ./ pressure_thermal
+    bra2 = L_32 .* IMAS.gradient(Te, rho) ./ Te
+    bra3 = L_34 .* alpha .* (1 .- R_pe) ./ R_pe .* IMAS.gradient(psi, Ti) ./ Ti
+
+
+    jboot1 = .-I_psi .* pe .* np.sign(q) .* (bra1 .+ bra2 .+ bra3)
+    return jboot1
 end
 
 """
