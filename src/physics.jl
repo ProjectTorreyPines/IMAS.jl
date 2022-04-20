30a05eca18881c3e4068f32d319844159c674138
Orso Meneghini <orso82@gmail.com>
2022-04-20 15:26:57 -0700

    Add pf_active coil volume calculation

--- src/physics.jl
+++ src/physics.jl
@@ -1502,6 +1502,15 @@ function area(coil::IMAS.pf_active__coil)
     return coil.element[1].geometry.rectangle.width * coil.element[1].geometry.rectangle.height
 end
 
+"""
+    volume(coil::IMAS.pf_active__coil)
+
+returns volume of PF coils
+"""
+function volume(coil::IMAS.pf_active__coil)
+    return area(coil) * 2pi * coil.element[1].geometry.rectangle.r
+end
+
 function energy_thermal(dd::IMAS.dd)
     return energy_thermal(dd.core_profiles.profiles_1d[])
 end
