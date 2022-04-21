6ab00376b0558b98a37d3e58148783e75baa867f
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2022-04-21 09:45:23 -0700

    geometric_midplane_line_averaged_density function

--- src/physics.jl
+++ src/physics.jl
@@ -1797,4 +1797,13 @@ function greenwald_density(eqt::IMAS.equilibrium__time_slice)
     return (eqt.global_quantities.ip/1e6) / (pi * eqt.boundary.minor_radius^2) * 1e20
 end
 
+"""
+    geometric_midplane_line_averaged_density(eqt,cp1d)
+
+Calculates the line averaged density from a midplane horizantal line
+"""
+
+function geometric_midplane_line_averaged_density(eqt::IMAS.equilibrium__time_slice, cp1d::IMAS.core_profiles__profiles_1d)
+    a_cp = IMAS.interp1d(eqt.profiles_1d.rho_tor_norm, (eqt.profiles_1d.r_outboard .- eqt.profiles_1d.r_inboard) / 2.0).(cp1d.grid.rho_tor_norm)
+    return integrate(a_cp,cp1d.electrons.density)/a_cp[end]
 end
