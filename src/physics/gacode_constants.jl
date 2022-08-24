a0093f04efa31099d53ea1c28aea6bc5fb18ce7b
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2022-08-24 15:41:11 -0700

    adding GACODE quantities and units

---
+++ src/physics/gacode_constants.jl
@@ -0,0 +1,42 @@
+const gacode_units = (
+    e=4.8032e-10, # stacoul
+    k=1.6022e-12, # erg/eV
+    me=9.1094e-28, #g
+    c=2.9979e10, # cm/s
+    m_to_cm=1e2,
+    T_to_Gauss=1e4
+)
+
+function c_s(cp1d::IMAS.core_profiles__profiles_1d)
+    return sqrt.(gacode_units.k .* cp1d.electrons.temperature ./ (cp1d.ion[1].element[1].a .* 1.6726e-24))
+end
+
+function rho_s(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    bunit = IMAS.interp1d(eqt.profiles_1d.rho_tor_norm,abs.(IMAS.bunit(eqt)) .* gacode_units.T_to_Gauss).(cp1d.grid.rho_tor_norm)
+    return c_s(cp1d) ./ (gacode_units.e .* bunit) .* (cp1d.ion[1].element[1].a .* 1.6726e-24 .* gacode_units.c)
+end
+
+function r_min_core_profiles(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    eq1d = eqt.profiles_1d
+    return IMAS.interp1d(eq1d.rho_tor_norm, gacode_units.m_to_cm * 0.5 * (eq1d.r_outboard - eq1d.r_inboard)).(cp1d.grid.rho_tor_norm)
+end
+
+##### Gyrobohm normalizations
+function gyrobohm_particle_flux(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    return cp1d.electrons.density_thermal.* 1e-6 .* c_s(cp1d) .* (rho_s(cp1d, eqt) ./ (eqt.boundary.minor_radius .* 100)) .^2 .* 1.0e-7
+end
+
+function gyrobohm_momentum_flux(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    return cp1d.electrons.density_thermal.* 1e-6 .* gacode_units.k .* cp1d.electrons.temperature .* eqt.boundary.minor_radius .* 100 .* (rho_s(cp1d, eqt) ./ (eqt.boundary.minor_radius .* 100)).^2 
+end
+
+function gyrobohm_energy_flux(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    a = (eqt.boundary.minor_radius .* 100)
+    return cp1d.electrons.density_thermal.* 1e-6 .* gacode_units.k .* cp1d.electrons.temperature .* c_s(cp1d) .* (rho_s(cp1d, eqt) ./ a).^2 .* 1.0e-7
+end #     input_tglf._Qgb = ne * k * Te * c_s * (rho_s / a)^2 * 1.0e-7
+
+
+function gyrobohm_exchange_powerdensity(cp1d::IMAS.core_profiles__profiles_1d, eqt::IMAS.equilibrium__time_slice)
+    return cp1d.electrons.density_thermal.* 1e-6 .* gacode_units.k .* cp1d.electrons.temperature .* (c_s(cp1d) ./ (eqt.boundary.minor_radius .* 100)) .* (rho_s(cp1d, eqt) ./ (eqt.boundary.minor_radius .* 100)).^2
+end
+
