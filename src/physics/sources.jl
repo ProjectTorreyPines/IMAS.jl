1ca0798c34bc1eea45e9dfa4db94e988b3c98fae
Tim Slendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2022-09-13 12:22:49 -0700

    little mistake prof_1d[1]

--- src/physics/sources.jl
+++ src/physics/sources.jl
@@ -180,7 +180,7 @@ end
 Calculates the number of D-D thermal fusion reactions to He3 in [reactions/m³/s]
 """
 function D_D_to_He3_reactions(dd::IMAS.dd)
-    index = findfirst(ion.label == "D" for ion in dd.core_profiles.profiles_1d[1].ion)
+    index = findfirst(ion.label == "D" for ion in dd.core_profiles.profiles_1d[].ion)
     @assert index !== nothing "There is no Deuterium only species in dd.core_profiles"
     dd_deut = dd.core_profiles.profiles_1d[].ion[index]
     return dd_deut.density_thermal.^2 .* reactivity(dd_deut.temperature, "D-DtoHe3") #  reactions/m³/s
