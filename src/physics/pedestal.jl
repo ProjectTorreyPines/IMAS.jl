e94a05ae76d3eaa1a682c003760b36fbd39f6f7b
Orso Meneghini <orso82@gmail.com>
2022-10-09 21:57:49 -0700

    new blend_core_edge also for Lmode

--- src/physics/pedestal.jl
+++ src/physics/pedestal.jl
@@ -1,5 +1,5 @@
 """
-    blend_core_pedestal_Hmode(
+    blend_core_edge_Hmode(
             profile::AbstractVector{<:Real},
             rho::AbstractVector{<:Real},
             ped_height::Real,
@@ -8,7 +8,7 @@
 
 Blends the core and pedestal for given profile to match ped_height, ped_width using nml_bound as blending boundary
 """
-function blend_core_pedestal_Hmode(
+function blend_core_edge_Hmode(
     profile::AbstractVector{<:Real},
     rho::AbstractVector{<:Real},
     ped_height::Real,
@@ -41,11 +41,11 @@ function blend_core_pedestal_Hmode(
 end
 
 """
-    blend_core_pedestal_Hmode(cp1d::IMAS.core_profiles__profiles_1d, dd_ped::IMAS.summary__local__pedestal)
+    blend_core_edge_Hmode(cp1d::IMAS.core_profiles__profiles_1d, dd_ped::IMAS.summary__local__pedestal, edge_bound::Real)
 
 Blends Te, Ti, ne, and nis in core_profiles with H-mode like pedestal defined in summary
 """
-function blend_core_pedestal_Hmode(cp1d::IMAS.core_profiles__profiles_1d, dd_ped::IMAS.summary__local__pedestal, nml_bound::Real=0.8)
+function blend_core_edge_Hmode(cp1d::IMAS.core_profiles__profiles_1d, dd_ped::IMAS.summary__local__pedestal, edge_bound::Real)
     rho = cp1d.grid.rho_tor_norm
     w_ped = 1 - @ddtime(dd_ped.position.rho_tor_norm)
 
@@ -54,10 +54,58 @@ function blend_core_pedestal_Hmode(cp1d::IMAS.core_profiles__profiles_1d, dd_ped
         ion_fractions[ii, :] = ion.density_thermal ./ cp1d.electrons.density
     end
 
-    cp1d.electrons.temperature = blend_core_pedestal_Hmode(cp1d.electrons.temperature, rho, @ddtime(dd_ped.t_e.value), w_ped, nml_bound; expin=1.2, expout=1.4)
-    cp1d.electrons.density_thermal = blend_core_pedestal_Hmode(cp1d.electrons.density, rho, @ddtime(dd_ped.n_e.value), w_ped, nml_bound; expin=1.1, expout=1.1)
+    cp1d.electrons.temperature = blend_core_edge_Hmode(cp1d.electrons.temperature, rho, @ddtime(dd_ped.t_e.value), w_ped, edge_bound; expin=1.2, expout=1.4)
+    cp1d.electrons.density_thermal = blend_core_edge_Hmode(cp1d.electrons.density, rho, @ddtime(dd_ped.n_e.value), w_ped, edge_bound; expin=1.1, expout=1.1)
 
-    ti_avg_new = blend_core_pedestal_Hmode(cp1d.ion[1].temperature, rho, @ddtime(dd_ped.t_i_average.value), w_ped, nml_bound; expin=1.2, expout=1.4)
+    ti_avg_new = blend_core_edge_Hmode(cp1d.ion[1].temperature, rho, @ddtime(dd_ped.t_i_average.value), w_ped, edge_bound; expin=1.2, expout=1.4)
+    for (ii, ion) in enumerate(cp1d.ion)
+        ion.density_thermal = ion_fractions[ii, :] .* cp1d.electrons.density
+        ion.temperature = ti_avg_new
+    end
+end
+
+"""
+    blend_core_edge_Lmode(
+        profile::AbstractVector{<:Real},
+        rho::AbstractVector{<:Real},
+        edge_bound::Real)
+
+Generates L-mode like profile by enforcing constant gradient between edge_bound and edge
+"""
+function blend_core_edge_Lmode(
+    profile::AbstractVector{<:Real},
+    rho::AbstractVector{<:Real},
+    edge_bound::Real)
+
+    inml = argmin(abs.(rho .- edge_bound))
+
+    grad_profile = -gradient(rho, profile; method=:backward)
+    grad_edge = grad_profile[inml]
+    edge = profile[end] .+ (1.0 .- rho) .* grad_edge
+
+    profile = profile .- profile[inml] .+ edge[inml]
+
+    profile[inml:end] = edge[inml:end]
+    return profile
+end
+
+"""
+    blend_core_edge_Lmode(cp1d::IMAS.core_profiles__profiles_1d)
+
+Blends Te, Ti, ne, and nis in core_profiles with L-mode like pedestal
+"""
+function blend_core_edge_Lmode(cp1d::IMAS.core_profiles__profiles_1d, edge_bound::Real)
+    rho = cp1d.grid.rho_tor_norm
+
+    ion_fractions = zeros(Float64, length(cp1d.ion), length(cp1d.electrons.density))
+    for (ii, ion) in enumerate(cp1d.ion)
+        ion_fractions[ii, :] = ion.density_thermal ./ cp1d.electrons.density
+    end
+
+    cp1d.electrons.temperature = blend_core_edge_Lmode(cp1d.electrons.temperature, rho, edge_bound)
+    cp1d.electrons.density_thermal = blend_core_edge_Lmode(cp1d.electrons.density, rho, edge_bound)
+
+    ti_avg_new = blend_core_edge_Lmode(cp1d.ion[1].temperature, rho, edge_bound)
     for (ii, ion) in enumerate(cp1d.ion)
         ion.density_thermal = ion_fractions[ii, :] .* cp1d.electrons.density
         ion.temperature = ti_avg_new
