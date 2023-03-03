31ef9fe1d259be5d4be3d2c16d90e68fda5adab0
Orso Meneghini <orso82@gmail.com>
2023-03-03 01:44:00 -0800

    digest v3

--- test/runtests_digest.jl
+++ test/runtests_digest.jl
@@ -4,8 +4,13 @@ using Test
 
 @testset "digest" begin
     filename = joinpath(dirname(dirname(pathof(IMASDD))), "sample", "D3D_eq_ods.json")
-    dd = IMAS.json2imas(filename; verbose = false)
+    dd = IMAS.json2imas(filename; verbose=false)
 
-    igest=IMAS.digest(dd)
-    println(igest)
+    dd.global_time = 0.0
+    dd.summary.time = [0.0]
+    dd.summary.line_average.n_e.value = [0.0]
+    dd.summary.local.magnetic_axis.n_e.value = [0.0]
+    igest = IMAS.digest(dd.summary)
+
+    igest = IMAS.digest(dd.equilibrium)
 end
