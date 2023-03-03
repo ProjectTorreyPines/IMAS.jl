edccbbb172203e1c171127294dc8de584072a2d1
Orso Meneghini <orso82@gmail.com>
2023-03-02 23:14:01 -0800

    digest v2

--- src/digest.jl
+++ src/digest.jl
@@ -1,53 +1,36 @@
-abstract type AbstractIDSdict end
+const DDigest = Dict{Symbol,String}()
 
-Base.@kwdef mutable struct DDigest <: AbstractIDSdict
-    _data::Dict{Symbol,Any} = Dict{Symbol,Any}()
+function add_ddigest(location::String)
+    ids_type = u2f(location)
+    return add_ddigest(ids_type)
 end
 
-function Base.getproperty(igest::AbstractIDSdict, field::Symbol)
-    data = getfield(igest,:_data)
-    if field ∉ keys(data)
-        data[field] = typeof(igest)()
-    else
-        return data[field]
-    end
-end
-
-function Base.setproperty!(igest::AbstractIDSdict, field::Symbol, value::Any)
-    data = getfield(igest, :_data)
-    if typeof(value) <: IDS
-        data[field] = typeof(igest)()
-        for (subfield,subvalue) in value
-            setproperty!(data[field], subfield, subvalue)
-        end
-    elseif 
-        data[field] = value
-    end
-end
-
-function Base.show(io::IO, igest::AbstractIDSdict)
-    return show(io, getfield(igest, :_data))
-end
-
-function Base.keys(igest::AbstractIDSdict)
-    return keys(getfield(igest,:_data))
-end
-
-#==========================#
-
-function digest(dd::IMAS.dd)
-    igest = DDigest()
-    if !isempty(dd.equilibrium.time_slice)
-        for (field,value) in dd.equilibrium.time_slice[].global_quantities
-            setproperty!(igest.equilibrium, field, value)
+function add_ddigest(@nospecialize(ids_type::Type{<:IDS}))
+    location = fs2u(ids_type)
+    out = Dict{Symbol,String}()
+    for (fname, ftype) in zip(fieldnames_(ids_type), fieldtypes_(ids_type))
+        floc = "$location.$fname"
+        if fname == :source
+            continue
+        elseif ftype <: IDS
+            tmp = add_ddigest(floc)
+            for k in keys(tmp)
+                if k == :value
+                    out[fname] = tmp[k]
+                else
+                    out[Symbol("$(fname)__$k")] = tmp[k]
+                end
+            end
+        elseif ftype <: IDSvector
+            #pass
+        else
+            out[fname] = "dd." * floc
         end
     end
-
-    if !isempty(dd.core_profiles.global_quantities)
-        for (field,value) in dd.core_profiles.global_quantities
-            setproperty!(igest.equilibrium, field, value)
-        end
-    end
-
-    return igest
+    return out
 end
+
+# empty!(IMAS.DDigest)
+# merge!(IMAS.DDigest, add_ddigest("summary.global_quantities"))
+# merge!(IMAS.DDigest, add_ddigest("equilibrium.time_slice[:].global_quantities"))
+# merge!(IMAS.DDigest, add_ddigest("core_profiles.global_quantities"))
