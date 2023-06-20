32e07efe58bd8c174227e5851cf91fbe1ec31b18
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-06-20 12:31:54 -0700

    add get_from function to IMAS
    used to get ip from a specified  location

---
+++ src/get_from.jl
@@ -0,0 +1,19 @@
+function get_from(dd::IMAS.dd, what::Symbol, from_where::Symbol)
+    return get_from(dd, Val{what}, from_where)
+end
+
+function get_from(dd::IMAS.dd, what::Type{Val{:ip}}, from_where::Symbol)
+    if from_where == :equilibrium
+        return dd.equilibrium.time_slice[].global_quantities.ip
+    elseif from_where == :core_profiles
+        return @ddtime(dd.core_profiles.global_quantities.ip)
+    elseif from_where == :pulse_schedule
+        return @ddtime(dd.pulse_schedule.flux_control.i_plasma.reference.data)
+    else
+        error("$what from $from_where doesn't exist yet")
+    end
+end
+
+function get_from(dd::IMAS.dd, what::Type{Val{:bt}}, from_where::Symbol)
+    return nothing
+end
