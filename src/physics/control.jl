b62d2a9cf813c4e2199f13874e2544923409677d
Orso Meneghini <orso82@gmail.com>
2024-02-07 21:25:04 -0800

    controller selector and pid_controller initializer

--- src/physics/control.jl
+++ src/physics/control.jl
@@ -1,10 +1,19 @@
 """
-    controllers__linear_controller(kP::T, kI::T, kD::T) where {T<:Real}
+    controller(ct::IMAS.controllers, name::String)
 
-Constructor that initializes SISO PID controller
+Pick a controller based on its name
 """
-function controllers__linear_controller(kP::T, kI::T, kD::T) where {T<:Real}
-    controller = controllers__linear_controller{T}()
+function controller(ct::IMAS.controllers, name::String)
+    index = findfirst(c -> c.name == name, ct.linear_controller)
+    return ct.linear_controller[index]
+end
+
+"""
+    pid_controller(controller::IMAS.controllers__linear_controller, kP::T, kI::T, kD::T) where {T<:Real}
+
+Initializes SISO PID controller
+"""
+function pid_controller(controller::IMAS.controllers__linear_controller, kP::T, kI::T, kD::T) where {T<:Real}
     controller.input_names = ["error"]
     controller.output_names = ["control"]
     controller.pid.p.time = [-Inf]
@@ -34,7 +43,10 @@ function (controller::controllers__linear_controller{T})(setpoint::T, value::T, 
         integral = zero(T)
         derivative = zero(T)
     else
-        integral = sum((controller.inputs.data[1, k+1] + controller.inputs.data[1, k]) / 2.0 * (controller.inputs.time[k+1] - controller.inputs.time[k]) for k in 1:length(controller.inputs.time)-1)
+        integral = sum(
+            (controller.inputs.data[1, k+1] + controller.inputs.data[1, k]) / 2.0 * (controller.inputs.time[k+1] - controller.inputs.time[k]) for
+            k in 1:length(controller.inputs.time)-1
+        )
         derivative = (controller.inputs.data[1, end] - controller.inputs.data[1, end-1]) / (controller.inputs.time[end] - controller.inputs.time[end-1])
     end
     control = (controller.pid.p.data[1] * error) + (controller.pid.i.data[1] * integral) + (controller.pid.d.data[1] * derivative)
