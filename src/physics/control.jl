22840c34f0d874b62d9f7aef7731a1676bebabda
Orso Meneghini <orso82@gmail.com>
2024-02-07 23:05:30 -0800

    controller with pubsub capability

--- src/physics/control.jl
+++ src/physics/control.jl
@@ -5,6 +5,9 @@ Pick a controller based on its name
 """
 function controller(ct::IMAS.controllers, name::String)
     index = findfirst(c -> c.name == name, ct.linear_controller)
+    if index === nothing
+        return nothing
+    end
     return ct.linear_controller[index]
 end
 
@@ -35,24 +38,51 @@ end
 Operates the linear controller
 """
 function (controller::controllers__linear_controller{T})(setpoint::T, value::T, time0::Float64) where {T<:Real}
-    error = setpoint - value
-    push!(controller.inputs.time, time0)
-    controller.inputs.data = hcat(controller.inputs.data, [error])
 
-    if size(controller.inputs.data)[2] < 2
-        integral = zero(T)
-        derivative = zero(T)
+    P = controller.pid.p.data[1]
+    I = controller.pid.i.data[1]
+    D = controller.pid.d.data[1]
+
+    dd = top_dd(controller)
+
+    if has_redis_controller(dd, controller.name)
+        @warn "Running REDIS controller"
+        control = redis_controller(dd, controller.name; time=time0, setpoint, value, P, I, D)[:control]
+
     else
-        integral = sum(
-            (controller.inputs.data[1, k+1] + controller.inputs.data[1, k]) / 2.0 * (controller.inputs.time[k+1] - controller.inputs.time[k]) for
-            k in 1:length(controller.inputs.time)-1
-        )
-        derivative = (controller.inputs.data[1, end] - controller.inputs.data[1, end-1]) / (controller.inputs.time[end] - controller.inputs.time[end-1])
+        error = setpoint - value
+        push!(controller.inputs.time, time0)
+        controller.inputs.data = hcat(controller.inputs.data, [error])
+
+        if size(controller.inputs.data)[2] < 2
+            integral = zero(T)
+            derivative = zero(T)
+        else
+            integral = sum(
+                (controller.inputs.data[1, k+1] + controller.inputs.data[1, k]) / 2.0 * (controller.inputs.time[k+1] - controller.inputs.time[k]) for
+                k in 1:length(controller.inputs.time)-1
+            )
+            derivative = (controller.inputs.data[1, end] - controller.inputs.data[1, end-1]) / (controller.inputs.time[end] - controller.inputs.time[end-1])
+        end
+        control = (P * error) + (I * integral) + (D * derivative)
     end
-    control = (controller.pid.p.data[1] * error) + (controller.pid.i.data[1] * integral) + (controller.pid.d.data[1] * derivative)
 
     push!(controller.outputs.time, time0)
     controller.outputs.data = hcat(controller.outputs.data, [control])
 
     return control
 end
+
+function redis_controller(dd::IMAS.dd, controller_name::String; kw...)
+    channel_fuse2ctrl = "$(controller_name)__fuse2ctrl"
+    channel_ctrl2fuse = "$(controller_name)__ctrl2fuse"
+    Jedis.publish(dd, channel_fuse2ctrl; kw...)
+    timeout = get(ENV, "FUSE_CONTROLLER_TIMEOUT", 1.0)
+    return listen_and_wait(dd, channel_ctrl2fuse; timeout)
+end
+
+function has_redis_controller(dd::IMAS.dd, controller_name::String)
+    channel_fuse2ctrl = "$(controller_name)__fuse2ctrl"
+    return pubsub(dd) && has_one_subscriber(dd, channel_fuse2ctrl)
+end
+
