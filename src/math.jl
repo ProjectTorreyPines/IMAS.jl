e8b78e838e309c845ecd00cc80250c790541bc4b
Orso Meneghini <orso82@gmail.com>
2024-03-12 10:02:43 -0700

    expose calc_z finite difference `method`
    mention https://github.com/ProjectTorreyPines/FUSE.jl/issues/527

--- src/math.jl
+++ src/math.jl
@@ -33,9 +33,9 @@ end
 """
     gradient(coord::AbstractVector{C}, arr::AbstractVector{A}; method::Symbol=:second_order) where {C<:Real, A<:Real}
 
-Finite difference method of the gradient: [:third_order, :second_order, :central, :backward, :forward]
+The finite difference `method` of the gradient can be one of [:third_order, :second_order, :central, :backward, :forward]
 
-The returned gradient hence has the same shape as the input array. https://numpy.org/doc/stable/reference/generated/numpy.gradient.html
+The returned gradient has the same shape as the input array https://numpy.org/doc/stable/reference/generated/numpy.gradient.html
 
 For `:central` the gradient is computed using second order accurate central differences in the interior points and first order accurate one-sides (forward or backward) differences at the boundaries.
 
@@ -547,7 +547,7 @@ end
         retain_original_xy::Bool=false,
         method::Symbol=:linear) where {T<:Real}
 
-Like resample_2d_path but with retain_extrema=true and method=linear as defaults
+Like resample_2d_path but with `retain_extrema=true` and `method=:linear` as defaults
 """
 function resample_plasma_boundary(
     x::AbstractVector{T},
@@ -732,14 +732,16 @@ function curvature(pr::AbstractVector{T}, pz::AbstractVector{T}) where {T<:Real}
 end
 
 """
-    calc_z(x::AbstractVector{<:Real}, f::AbstractVector{<:Real})
+    calc_z(x::AbstractVector{<:Real}, f::AbstractVector{<:Real}; method::Symbol=:third_order)
 
 Returns the gradient scale lengths of vector f on x
 
+The finite difference `method` of the gradient can be one of [:third_order, :second_order, :central, :backward, :forward]
+
 NOTE: the inverse scale length is NEGATIVE for typical density/temperature profiles
 """
-function calc_z(x::AbstractVector{<:Real}, f::AbstractVector{<:Real})
-    return gradient(x, f; method=:third_order) ./ f
+function calc_z(x::AbstractVector{<:Real}, f::AbstractVector{<:Real}; method::Symbol=:third_order)
+    return gradient(x, f; method) ./ f
 end
 
 """
