119aa6075178f232041c1f3077eaf079ae29d410
Giacomo Dose <giacomo.dose@gmail.com>
2024-03-13 15:39:58 -0700

    Comments + new hf entries

--- src/hf_mapper.jl
+++ src/hf_mapper.jl
@@ -1,9 +1,11 @@
 mutable struct WallHeatFlux
-    r::Vector{Float64}
-    z::Vector{Float64}
-    q_wall::Vector{Float64}
-    q_parallel::Vector{Float64}
-    s::Vector{Float64}
+    r::Vector{Float64}                  # R coordinate of the wall mesh                     - m
+    z::Vector{Float64}                  # Z coordinate of the wall mesh                     - m
+    q_wall::Vector{Float64}             # total heat flux on the wall                       - W/m2
+    q_part::Vector{Float64}             # heat flux on the wall due to particles            - W/m2
+    q_core_rad::Vector{Float64}         # heat flux on the wall due to core radiation       - W/m2
+    q_parallel::Vector{Float64}         # parallel heat flux due to particles at the wall   - W/m2
+    s::Vector{Float64}                  # wall curvilinear abscissa                         - m
 end
 
 function WallHFMapper(eqt::IMAS.equilibrium__time_slice, 
