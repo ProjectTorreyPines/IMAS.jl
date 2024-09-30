e705ecab7ee1e34b1cd9916c54ba5bc52cff88c2
Giacomo Dose <giacomo.dose@gmail.com>
2024-09-30 16:34:12 -0700

    Fix bug in find_strike_points: use always :any
    if surface is :open this returns the line inside the wall, the intersection is not always found (did not understand why). Therefore the strike_points are not found. If one uses :any the surface extends into the wall and the strike points are always found.

--- src/physics/sol.jl
+++ src/physics/sol.jl
@@ -934,11 +934,7 @@ function find_strike_points(
     if !isempty(wall_r)
         # find separatrix as first surface in SOL, not in private region
         if psi_first_open !== nothing
-            if private_flux_regions
-                sep = flux_surface(eqt, psi_first_open, :any, Float64[], Float64[])
-            else
-                sep = flux_surface(eqt, psi_first_open, :open, Float64[], Float64[])
-            end
+            sep = flux_surface(eqt, psi_first_open, :any, Float64[], Float64[])
             zaxis = eqt.boundary.geometric_axis.z
             for (pr, pz) in sep
                 if isempty(pr)
