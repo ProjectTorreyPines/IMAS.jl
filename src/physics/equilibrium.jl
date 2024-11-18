335561f78ce8650a324450404703860065c2561b
Orso Meneghini <orso82@gmail.com>
2024-11-18 13:06:56 -0800

    document physics equilibrium

--- src/physics/equilibrium.jl
+++ src/physics/equilibrium.jl
@@ -1,3 +1,5 @@
+document[Symbol("Physics equilibrium")] = Symbol[]
+
 """
     calc_pprime_ffprim_f(
         psi::T,
@@ -14,22 +16,21 @@
         f_df_dpsi::Union{Missing,T}=missing,
         ) where {T<:AbstractVector{<:Real}}
 
-This method returns the P' and FF' given P or P' and J or J/R based on the current equilibrium fluxsurfaces geometry
+This method returns the `P'` and `FF'` given `P` or `P'` and `J` or `J/R` based on the current equilibrium fluxsurfaces geometry
 
 Arguments:
-psi::T,
-
-  - R: <R>
-  - one_R: <1/R>
-  - one_R2: <1/R²>
-  - R0: R at which B0 is defined
-  - B0: vacuum magnetic field
-  - pressure: P
-  - dpressure_dpsi: P'
-  - j_tor: toroidal current
-  - j_tor_over_R: flux surface averaged toroidal current density over major radius
-  - f: F
-  - f_df_dpsi: FF'
+* `psi`: poloidal flux
+* `R`: <R>
+* `one_R`: <1/R>
+* `one_R2`: <1/R²>
+* `R0`: R at which B0 is defined
+* `B0`: vacuum magnetic field
+* `pressure`: P
+* `dpressure_dpsi`: P'
+* `j_tor`: toroidal current
+* `j_tor_over_R`: flux surface averaged toroidal current density over major radius
+* `f`: F
+* `f_df_dpsi`: FF'
 
 returns (P', FF', F)
 """
@@ -78,9 +79,17 @@ function calc_pprime_ffprim_f(
         f = sqrt.(abs.(f .+ C))
     end
 
-    return dpressure_dpsi, f_df_dpsi, f
+    return (dpressure_dpsi=dpressure_dpsi, f_df_dpsi=f_df_dpsi, f=f)
 end
 
+@compat public calc_pprime_ffprim_f
+push!(document[Symbol("Physics equilibrium")], :calc_pprime_ffprim_f)
+
+"""
+    p_jtor_2_pprime_ffprim_f(eqt1::IMAS.equilibrium__time_slice___profiles_1d, R0::Real, B0::Real)
+
+Calculate `P'`, `FF'` and `F` from `pressure` and `j_tor` in equilibrium
+"""
 function p_jtor_2_pprime_ffprim_f(eqt1::IMAS.equilibrium__time_slice___profiles_1d, R0::Real, B0::Real)
     psi = eqt1.psi
     R = eqt1.gm8
@@ -93,13 +102,8 @@ function p_jtor_2_pprime_ffprim_f(eqt1::IMAS.equilibrium__time_slice___profiles_
     return calc_pprime_ffprim_f(psi, R, one_R, one_R2, R0, B0; presssure, j_tor)
 end
 
-function p_jtor_2_pprime_ffprim_f!(eqt1::IMAS.equilibrium__time_slice___profiles_1d, R0::Real, B0::Real)
-    dpressure_dpsi, f_df_dpsi, f = p_jtor_2_pprime_ffprim_f(eqt1, R0, B0)
-    eqt1.dpressure_dpsi = dpressure_dpsi
-    eqt1.f_df_dpsi = f_df_dpsi
-    eqt1.f = f
-    return dpressure_dpsi, f_df_dpsi, f
-end
+@compat public p_jtor_2_pprime_ffprim_f
+push!(document[Symbol("Physics equilibrium")], :p_jtor_2_pprime_ffprim_f)
 
 """
     symmetrize_equilibrium!(eqt::IMAS.equilibrium__time_slice)
@@ -131,13 +135,20 @@ function symmetrize_equilibrium!(eqt::IMAS.equilibrium__time_slice)
     return eqt
 end
 
+@compat public symmetrize_equilibrium!
+push!(document[Symbol("Physics equilibrium")], :symmetrize_equilibrium!)
+
 """
     B0_geo(eqt::IMAS.equilibrium__time_slice{T})::T where{T<:Real}
 
-Returns vacuum B0 at the plasma geometric center
+Returns vacuum B0 at the plasma geometric center, which may or may not be `eqt.global_quantities.vacuum_toroidal_field.r0`
 """
 function B0_geo(eqt::IMAS.equilibrium__time_slice{T})::T where {T<:Real}
-    R0, B0 = eqt.global_quantities.vacuum_toroidal_field.r0, eqt.global_quantities.vacuum_toroidal_field.b0
+    r0 = eqt.global_quantities.vacuum_toroidal_field.r0
+    b0 = eqt.global_quantities.vacuum_toroidal_field.b0
     Rgeo = eqt.boundary.geometric_axis.r
-    return R0 .* B0 ./ Rgeo
+    return r0 .* b0 ./ Rgeo
 end
+
+@compat public B0_geo
+push!(document[Symbol("Physics equilibrium")], :B0_geo)
