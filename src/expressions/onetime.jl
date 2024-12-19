5ffaa632568624b67c53bfbf5f809cf7b8098ef3
Min-Gu Yoo <mgyoo86@gmail.com>
2024-12-19 13:17:37 -0800

    Define new macro: `@store_expr`

--- src/expressions/onetime.jl
+++ src/expressions/onetime.jl
@@ -29,6 +29,35 @@ function Base.show(io::IO, ::MIME"text/plain", expr_info::ExprInfo)
     return print(io, "$(expr_info.body)\n")
 end
 
+"""
+    @store_expr expr
+
+Takes an assignment expression of the form `dict["key"] = (args...) -> body` and:
+
+ 1. Executes the original assignment.
+ 2. Extracts the argument list and body from the given anonymous function.
+ 3. Stores this information as an `ExprInfo` object in `expr_info_dict` under the specified key.
+"""
+macro store_expr(expr)
+    @assert expr.head == :(=)
+    lhs = expr.args[1]
+    rhs = expr.args[2]
+
+    @assert lhs.head == :ref && length(lhs.args) == 2
+    key_expr = lhs.args[2]
+
+    expr_args = sprint(show, MacroTools.prettify(rhs.args[1]))
+    expr_body = sprint(show, MacroTools.prettify(rhs.args[2]))
+
+    expr_info_dict = :expr_info_dict
+
+    return quote
+        # execute the given original expression
+        $(esc(expr))
+        # store the ExprInfo into expr_info_dict
+        $expr_info_dict[$(key_expr)] = ExprInfo($key_expr, $expr_args, $expr_body)
+    end
+end
 # These expressions are frozen the first time they are accessed.
 # This is necessary to ensure that core_profiles, core_sources, and core_transport grids do not change after changing the equilibrium.
 # The idea is that we want to freeze in the core_profiles, core_sources, and core_transport grids the rho, psi, volume, area, ... info that were used when those IDSs were filled.
