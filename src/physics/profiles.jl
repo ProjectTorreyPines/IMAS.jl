6f2c99dd61de0bf5ae344d762acc2a9709b5cf47
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2025-01-28 12:56:55 -0800

    add threshold_multiple

--- src/physics/profiles.jl
+++ src/physics/profiles.jl
@@ -846,10 +846,11 @@ push!(document[Symbol("Physics profiles")], :L_H_threshold)
 
 Returns `true` if the plasma is diverted, has positive triangularity, and `Psol>Plh`
 """
-function satisfies_h_mode_conditions(dd::IMAS.dd)
+function satisfies_h_mode_conditions(dd::IMAS.dd; threshold_multiple::Float64=1.0)
     eqt = dd.equilibrium.time_slice[]
     diverted = length(eqt.boundary.x_point) > 0
-    Psol_gt_Plh = IMAS.L_H_threshold(dd) > getproperty(dd.requirements, :lh_power_threshold_fraction, 1.0)
+    LH_transition_fraction = getproperty(dd.requirements, :lh_power_threshold_fraction, 1.0)
+    Psol_gt_Plh = IMAS.L_H_threshold(dd) > LH_transition_fraction * multiple
     positive_triangularity = eqt.boundary.triangularity > 0.0
     if Psol_gt_Plh && diverted && positive_triangularity
         return true
