aea82bf8667aabc1bdee8a1f9ea38142dd4b1d10
Orso Meneghini <orso82@gmail.com>
2025-03-24 16:00:00 -0700

    improved plotting of controller data

--- src/plot.jl
+++ src/plot.jl
@@ -2190,7 +2190,7 @@ end
 
 @recipe function plot_nb(nb::IMAS.nbi{T}) where {T<:Real}
     @series begin
-        [unit.power_launched for unit in nb.unit if sum(unit.power_launched.data)> 0.0]
+        [unit.power_launched for unit in nb.unit if sum(unit.power_launched.data) > 0.0]
     end
 end
 
@@ -2797,7 +2797,7 @@ const UnionPulseScheduleSubIDS = Union{IMAS.pulse_schedule,(tp for tp in fieldty
                 eqt
             end
         end
-    
+
         wall = try
             top_dd(pc).wall
         catch
@@ -2900,67 +2900,77 @@ end
 #= =========== =#
 #  controllers  #
 #= =========== =#
-@recipe function plot_controllers(controller_outputs::controllers__linear_controller___outputs)
+@recipe function plot_controllers(controller_outputs::controllers__linear_controller___outputs, k::Int)
     controller = IMAS.parent(controller_outputs)
 
     data = controller.inputs.data[1, :]
-    integral = cumsum((data[k+1] + data[k]) / 2.0 * (controller.inputs.time[k+1] - controller.inputs.time[k]) for k in eachindex(controller.inputs.time) - 1)
-    derivative = diff(data) ./ diff(controller.inputs.time)
+    integral = cumtrapz(controller.inputs.time, data)
+    derivative = [0.0, diff(data) ./ diff(controller.inputs.time)]
 
     @series begin
-        label := "PID"
+        label := "Output"
         color := :black
         lw := 2
         controller.outputs.time, controller.outputs.data[1, :]
     end
 
     @series begin
-        label := "P=$(controller.pid.p.data[1])"
-        controller.outputs.time, data .* controller.pid.p.data[1]
+        label := "P=$(join(map(x->@sprintf("%.3e", x), @ddtime(controller.pid.p.data)),", "))"
+        controller.outputs.time, data .* controller.pid.p.data[k]
     end
 
     @series begin
-        label := "I=$(controller.pid.i.data[1])"
-        controller.outputs.time[2:end], integral .* controller.pid.i.data[1]
+        label := "I=$(join(map(x->@sprintf("%.3e", x), @ddtime(controller.pid.i.data)),", "))"
+        controller.outputs.time, integral .* controller.pid.i.data[k]
     end
 
-    @series begin
-        label := "D=$(controller.pid.d.data[1])"
-        controller.outputs.time[2:end], derivative .* controller.pid.d.data[1]
+    if any(controller.pid.d.data[k] .!= 0.0)
+        @series begin
+            label := "D=$(join(map(x->@sprintf("%.3e", x), @ddtime(controller.pid.d.data)),", "))"
+            controller.outputs.time, derivative .* controller.pid.d.data[k]
+        end
     end
 
-    @series begin
-        label := "PI"
-        controller.outputs.time[2:end], data[2:end] .* controller.pid.p.data[1] .+ integral .* controller.pid.i.data[1]
+    if any(controller.pid.i.data[k] .!= 0.0)
+        @series begin
+            label := "PI"
+            controller.outputs.time, data .* controller.pid.p.data[k] .+ integral .* controller.pid.i.data[k]
+        end
     end
 
-    @series begin
-        label := "PID"
-        controller.outputs.time[2:end], data[2:end] .* controller.pid.p.data[1] .+ integral .* controller.pid.i.data[1] .+ derivative .* controller.pid.d.data[1]
+    if any(controller.pid.d.data[k] .!= 0.0) && any(controller.pid.i.data[k] .!= 0.0)
+        @series begin
+            label := "PID"
+            controller.outputs.time, data .* controller.pid.p.data[k] .+ integral .* controller.pid.i.data[k] .+ derivative .* controller.pid.d.data[k]
+        end
     end
 
 end
 
-@recipe function plot_controllers(controller_inputs::controllers__linear_controller___inputs)
+@recipe function plot_controllers(controller_inputs::controllers__linear_controller___inputs, k::Int)
     controller = IMAS.parent(controller_inputs)
-    label = "P=$(controller.pid.p.data[1]), I=$(controller.pid.i.data[1]), D=$(controller.pid.d.data[1])"
     @series begin
-        label := "$(controller.input_names[1]) ($label)"
-        controller.inputs.time, controller.inputs.data[1, :]
+        label := "Input $(controller.input_names[k])"
+        controller_inputs.time, controller_inputs.data[k, :]
     end
 end
 
 @recipe function plot_controllers(controller::controllers__linear_controller)
-    layout := (2, 1)
-    size --> (800, 600)
+    n = size(controller.inputs.data)[1] + size(controller.outputs.data)[1]
+    layout := (n, 1)
+    size --> (800, n * 300)
     margin --> 5 * Measures.mm
-    @series begin
-        subplot := 1
-        controller.inputs
+    for k in 1:size(controller.inputs.data)[1]
+        @series begin
+            subplot := 1
+            controller.inputs, k
+        end
     end
-    @series begin
-        subplot := 2
-        controller.outputs
+    for k in 1:size(controller.outputs.data)[1]
+        @series begin
+            subplot := 2
+            controller.outputs, k
+        end
     end
 end
 
