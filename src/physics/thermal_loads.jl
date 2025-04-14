4ef57a0bef20c72f685bf5e965791a7e0c7e3a66
Giacomo Dose <giacomo.dose@gmail.com>
2025-04-14 15:42:00 -0700

    Overhaul of IMAS.particle_heat_flux
    Avoid repetition of code (with mesher), less allocations

--- src/physics/thermal_loads.jl
+++ src/physics/thermal_loads.jl
@@ -21,324 +21,64 @@ end
 
 """
     particle_heat_flux(
-        eqt::IMAS.equilibrium__time_slice,
         SOL::OrderedCollections.OrderedDict{Symbol,Vector{OpenFieldLine}},
-        wall_r::AbstractVector{<:Real},
-        wall_z::AbstractVector{<:Real},
+        rmesh::AbstractVector{<:Real},
+        zmesh::AbstractVector{<:Real},
         r::Vector{<:Real},
-        q::Vector{<:Real};
-        merge_wall::Bool=true)
+        q::Vector{<:Real})
 
-Computes the heat flux on the wall due to the influx of charged particles, using the magnetic equilibrium,
-the Scrape Off-Layer, the wall, and an hypothesis of the decay of the parallel heat flux at the OMP
+Computes the heat flux on the wall due to the influx of charged particles using
+the Scrape Off-Layer, the mesh, and an hypothesis of the decay of the parallel heat flux at the OMP.
+
+Assumption: Points at the extrema of OpenFieldLines in SOL are in the mesh (see mesher_heat_flux).
 """
 function particle_heat_flux(
-    eqt::IMAS.equilibrium__time_slice,
-    SOL::OrderedCollections.OrderedDict{Symbol,Vector{OpenFieldLine}},
-    wall_r::AbstractVector{<:Real},
-    wall_z::AbstractVector{<:Real},
-    r::Vector{<:Real},
-    q::Vector{<:Real};
-    merge_wall::Bool=true)
+        SOL::OrderedCollections.OrderedDict{Symbol,Vector{OpenFieldLine}},
+        rmesh::AbstractVector{<:Real},
+        zmesh::AbstractVector{<:Real},
+        r::Vector{<:Real},
+        q::Vector{<:Real})
 
-    @assert length(r) == length(q)
-    @assert all(q .>= 0) # q is all positive
-    @assert all(r .>= 0) # r is all positive  
-    @assert r[1] == 0
+        @assert length(r) == length(q)
+        @assert all(q .>= 0) # q is all positive
+        @assert all(r .>= 0) # r is all positive  
+        @assert r[1] == 0
 
-    Rwall = Float64[]
-    Zwall = Float64[]
-    Qwall = Float64[]
-    Qpara = Float64[]
-    indexes = Int64[]
+        # initialize outputs and set default value to zero. 
+        # Heat flux is not zero only in the points where field lines in SOL intersect
+        Qpara = zeros(length(rmesh))
+        Qwall = zeros(length(rmesh))
 
-    if isempty(wall_r) || isempty(wall_z)
-        error("Impossible to map the heat flux onto the wall because dd.wall is empty")
-    end
+        crossings = intersection([1, 10].*(minimum(rmesh) + maximum(rmesh))./2, [0.0, 0.0], rmesh, zmesh)
+        Romp_wall = crossings.crossings[1][1]
 
-    ZA = eqt.global_quantities.magnetic_axis.z # Z of magnetic axis
-    RA = eqt.global_quantities.magnetic_axis.r # R of magnetic axis
+        # Set interpolant of q(r)
+        r_separatrix = SOL[:lfs][1].r[SOL[:lfs][1].midplane_index] # Romp of LCFS
+        r = r .+ r_separatrix 
+        @assert maximum(r) > maximum(Romp_wall) "Vector r in q(r) is too short for interpolation. [Point of contact: G.Dose]"
 
-    eqt2d = findfirst(:rectangular, eqt.profiles_2d)
-    psi_separatrix = find_psi_boundary(eqt, wall_r, wall_z; raise_error_on_not_open=true).first_open # psi at LCFS
-    surface = flux_surface(eqt, psi_separatrix, :open, wall_r, wall_z)
-    r_separatrix = Float64[]
-    for (rr, zz) in surface
-        if isempty(rr) || all(zz .> ZA) || all(zz .< ZA)
-            continue
+        q_interp = interp1d(r, q, :cubic)
+
+        # Only SOL[:lfs] and SOL[:lfs_far] have power flowing inside
+        for sol in [SOL[:lfs]; SOL[:lfs_far]]
+            rmid = sol.r[sol.midplane_index] # Romp of surface sol
+            qmid = q_interp(rmid)
+
+            @assert qmid > 0 "qmid less than zero. Check q(r) interpolator. [Point of contact: G.Dose]" 
+
+            strike_point1 =  (rmesh .== sol.r[1])   .&&  (zmesh .== sol.z[1])   # index in mesh of first strike point of sol
+            strike_point2 =  (rmesh .== sol.r[end]) .&&  (zmesh .== sol.z[end]) # index in mesh of second strike point of sol
+
+            # compute heat flux for point 1
+            Qpara[strike_point1] .= qmid / (sol.total_flux_expansion[1])
+            Qwall[strike_point1] .= qmid / (sol.total_flux_expansion[1]) * sin(sol.grazing_angles[1])
+
+            # compute heat flux for point 2
+            Qpara[strike_point2] .= qmid / (sol.total_flux_expansion[end])
+            Qwall[strike_point2] .= qmid / (sol.total_flux_expansion[end]) * sin(sol.grazing_angles[end])
         end
 
-        crossings = intersection(rr, zz, [RA, 10 * RA], [ZA, ZA]).crossings # find intersection with midplane
-
-        if isempty(crossings)
-            continue
-        end
-
-        push!(r_separatrix, crossings[1][1])
-    end
-    r_separatrix = r_separatrix[1]
-    r = r .+ r_separatrix
-    @assert maximum(r) > maximum(wall_r)
-
-    q_interp = interp1d(r, q, :cubic)
-
-    # to determine if upper or lower single null, check if Z(strike_point) < Z(axis)
-    # SOL[:lfs][1] = LCFS
-    if SOL[:lfs][1].z[1]< ZA
-        case = :lower
-    else
-        case = :upper
-    end
-    if isempty(SOL[:lfs_far])
-        case = :double
-    end
-
-    # lower single null case
-    if case == :lower || case == :upper
-        #order clockwise starting from midplane
-        for sol in reverse(SOL[:lfs_far])
-            qmid = q_interp(sol.r[sol.midplane_index]) # compute parallell heat flux at omp
-            push!(Rwall, sol.r[end]) # R of points after midplane
-            push!(Zwall, sol.z[end]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[end]) * sin(sol.grazing_angles[end]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[end]))
-            push!(indexes, sol.wall_index[end])
-        end
-        for sol in reverse(SOL[:lfs])
-            # Outer target
-            qmid = q_interp(sol.r[sol.midplane_index])
-            push!(Rwall, sol.r[end]) # R of points after midplane
-            push!(Zwall, sol.z[end]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[end]) * sin(sol.grazing_angles[end]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[end]))
-            push!(indexes, sol.wall_index[end])
-        end
-        for sol in SOL[:lfs]
-            # Inner target
-            qmid = q_interp(sol.r[sol.midplane_index]) # compute parallell heat flux at omp
-            push!(Rwall, sol.r[1]) # R of points after midplane
-            push!(Zwall, sol.z[1]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[1]) * sin(sol.grazing_angles[1]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[1]))
-            push!(indexes, sol.wall_index[1])
-        end
-
-        for sol in SOL[:lfs_far]
-            qmid = q_interp(sol.r[sol.midplane_index]) # compute parallell heat flux at omp
-            push!(Rwall, sol.r[1]) # R of points after midplane
-            push!(Zwall, sol.z[1]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[1]) * sin(sol.grazing_angles[1]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[1]))
-            push!(indexes, sol.wall_index[1])
-        end
-
-        Rwall_hfs = Float64[]
-        Zwall_hfs = Float64[]
-        Qwall_hfs = Float64[]
-        Qpara_hfs = Float64[]
-        indexes_hfs = Int64[]
-
-        if !isempty(SOL[:hfs])
-            for sol in SOL[:hfs]
-                #order clockwise starting from midplane
-                push!(Rwall_hfs, sol.r[1]) # R of points after midplane
-                push!(Zwall_hfs, sol.z[1]) # z of points after midplane
-                push!(Qwall_hfs, 0.0)
-                push!(Qpara_hfs, 0.0)
-                push!(indexes_hfs, sol.wall_index[1])
-            end
-            for sol in reverse(SOL[:hfs])
-                push!(Rwall_hfs, sol.r[end]) # R of points after midplane
-                push!(Zwall_hfs, sol.z[end]) # z of points after midplane
-                push!(Qwall_hfs, 0.0)
-                push!(Qpara_hfs, 0.0)
-                push!(indexes_hfs, sol.wall_index[end])
-            end
-
-            # insert hfs
-            Rwall = vcat(Rwall[1:argmin(abs.(indexes .- maximum(indexes_hfs)))-1],
-                Rwall_hfs,
-                Rwall[argmin(abs.(indexes .- maximum(indexes_hfs))):end])
-            Zwall = vcat(Zwall[1:argmin(abs.(indexes .- maximum(indexes_hfs)))-1],
-                Zwall_hfs,
-                Zwall[argmin(abs.(indexes .- maximum(indexes_hfs))):end])
-            Qwall = vcat(Qwall[1:argmin(abs.(indexes .- maximum(indexes_hfs)))-1],
-                Qwall_hfs,
-                Qwall[argmin(abs.(indexes .- maximum(indexes_hfs))):end])
-            Qpara = vcat(Qpara[1:argmin(abs.(indexes .- maximum(indexes_hfs)))-1],
-                Qpara_hfs,
-                Qpara[argmin(abs.(indexes .- maximum(indexes_hfs))):end])
-            indexes = vcat(indexes[1:argmin(abs.(indexes .- maximum(indexes_hfs)))-1],
-                indexes_hfs,
-                indexes[argmin(abs.(indexes .- maximum(indexes_hfs))):end])
-        end
-    end
-
-    # double null case - SOL[:lfs_far] is empty
-    if case == :double
-        #order clockwise starting from midplane
-        for sol in reverse(SOL[:lfs])
-            qmid = q_interp(sol.r[sol.midplane_index]) # compute parallell heat flux at omp
-            push!(Rwall, sol.r[end]) # R of points after midplane
-            push!(Zwall, sol.z[end]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[end]) * sin(sol.grazing_angles[end]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[end]))
-            push!(indexes, sol.wall_index[end])
-        end
-        for sol in SOL[:hfs]
-            #order clockwise starting from midplane
-            push!(Rwall, sol.r[1]) # R of points after midplane
-            push!(Zwall, sol.z[1]) # z of points after midplane
-            push!(Qwall, 0)
-            push!(Qpara, 0)
-            push!(indexes, sol.wall_index[1])
-        end
-        for sol in reverse(SOL[:hfs])
-            push!(Rwall, sol.r[end]) # R of points after midplane
-            push!(Zwall, sol.z[end]) # z of points after midplane
-            push!(Qwall, 0)
-            push!(Qpara, 0)
-            push!(indexes, sol.wall_index[end])
-        end
-        for sol in SOL[:lfs]
-            qmid = q_interp(sol.r[sol.midplane_index]) # compute parallell heat flux at omp
-            push!(Rwall, sol.r[1]) # R of points after midplane
-            push!(Zwall, sol.z[1]) # z of points after midplane
-            push!(Qwall, qmid / (sol.total_flux_expansion[1]) * sin(sol.grazing_angles[1]))
-            push!(Qpara, qmid / (sol.total_flux_expansion[1]))
-            push!(indexes, sol.wall_index[1])
-        end
-    end
-
-    if merge_wall
-        _, _, PSI_interpolant = ψ_interpolant(eqt2d)  #interpolation of PSI in equilirium at locations (r,z)
-
-        #! format: off
-        if wall_z[1]>ZA # correction if first point is above midplane 
-            indexes[indexes .== 1 .&& Zwall.>ZA] .= length(wall_r) # put it after index = end  
-            indexes[indexes .== 1 .&& Zwall.>wall_z[2] .&& Zwall.<=ZA] .= 0   # put before index = 1
-        end
-        #! format: on
-
-        crossings = intersection([(minimum(wall_r) + maximum(wall_r)) / 2, maximum(wall_r) * 1.05], [ZA, ZA], wall_r, wall_z).crossings # (r,z) point of intersection btw outer midplane (OMP) with wall
-        r_wall_midplane = crossings[1][1] # R coordinate of the wall at OMP
-        psi_wall_midplane = PSI_interpolant(r_wall_midplane, ZA)
-        _, psi_first_lfs_far, null_within_wall = find_psi_last_diverted(eqt, wall_r, wall_z, PSI_interpolant) # psi of grazing surface
-        psi_wall = PSI_interpolant.(wall_r, wall_z)
-        tollZ = max(abs(ZA), 2 * abs(wall_z[2] - wall_z[1]))
-        if Zwall[1] >= -tollZ # check if the particle reach the wall around the midplane
-            # if so, do not add any point
-            add_omp = false
-        else
-            # add points
-            add_omp = true
-        end
-
-        #! format: off
-        add_indexes = collect((1:length(psi_wall)))
-        add_indexes = add_indexes[(psi_wall .< psi_separatrix .|| psi_wall .> psi_wall_midplane) .|| # add private flux region  around first null (psi<psi_sep) + add everyhting above psi midplane
-                                (psi_wall .>= psi_separatrix .&& psi_wall .<= psi_first_lfs_far .&&  # add also points inside the private region around second null (only if null is within wall)
-                                sign(eqt.boundary.x_point[end].z).*wall_z.>abs(eqt.boundary.x_point[end].z)).&& null_within_wall .||
-                                psi_wall .> psi_first_lfs_far .&& (wall_r.<eqt.boundary.x_point[end].r) .|| # add point in :hfs 
-                                (psi_wall .< psi_wall_midplane .&& (wall_z.<=tollZ) .&& (wall_z .>= -tollZ) .&& (wall_r.>eqt.boundary.x_point[end].r) .&& add_omp) # add also points close to OMP but with psi lower than psi_wall midplane within tollZ from omp
-                                ] 
-        L = length(add_indexes)
-        #! format: on
-
-        average_step = sum(sqrt.(diff(wall_r) .^ 2 + diff(wall_z) .^ 2)) ./ (length(wall_r) - 1)
-        #filter out point that are for some reason already inside Rwall,Zwall
-        for (k, ind) in enumerate(reverse(add_indexes))
-            #check if these points have been already saved in Rwall, Zwall
-            dist = sqrt.((Rwall .- wall_r[ind]) .^ 2 + (Zwall .- wall_z[ind]) .^ 2)
-            if minimum(dist) <= average_step # if a point is at less than average_step  from an already saved point, do not save it
-                #point is already in and must be removed
-                deleteat!(add_indexes, L + 1 - k)
-            end
-        end
-
-        add_indexes = reverse!(add_indexes)
-
-        for ind in add_indexes
-            if ind != 1
-                Rwall = append!(Rwall[indexes.<ind], [wall_r[ind]], Rwall[indexes.>=ind])
-                Zwall = append!(Zwall[indexes.<ind], [wall_z[ind]], Zwall[indexes.>=ind])
-                Qwall = append!(Qwall[indexes.<ind], [0.0], Qwall[indexes.>=ind])
-                Qpara = append!(Qpara[indexes.<ind], [0.0], Qpara[indexes.>=ind])
-                indexes = append!(indexes[indexes.<ind], [ind], indexes[indexes.>=ind])
-            end
-        end
-
-        # include points shadowed by corners
-        dist = (diff(Rwall)) .^ 2 + (diff(Zwall)) .^ 2
-        avg = sum(dist) / length(dist) # mean distance between adjacent points in mesh
-        std = sqrt(sum((dist .- avg) .^ 2) / (length(dist) - 1)) #standard deviation of distance adjacent points in mesh
-        save_length = length(Rwall)
-        counter = 0
-        while sum(dist .> avg + 4 * std) > 0 && counter < 10
-            # we have outliers, which are shadowed areas.
-            # in the outliears dist > average + 4 standard deviations
-            indexes = 1:length(dist)
-            indexes = indexes[dist.>avg+4*std]
-
-            for ind in reverse(indexes)
-                # if vertical lines
-                if abs.(diff([Rwall[ind], Rwall[ind+1]]))[1] > 0.01
-                    dr = 0.0
-                else
-                    # dr = 1.0*maximum(sqrt.(diff(wall_r).^2 + diff(wall_z).^2))
-                    dr = sqrt(2) * sum(sqrt.(diff(wall_r) .^ 2 + diff(wall_z) .^ 2)) / length(dist)
-                end
-                #if horizontal lines
-                if abs.(diff([Zwall[ind], Zwall[ind+1]]))[1] > 0.01
-                    dz = 0.0
-                else
-                    # dz = 1.0*maximum(sqrt.(diff(wall_r).^2 + diff(wall_z).^2))
-                    dz = sqrt(2) * sum(sqrt.(diff(wall_r) .^ 2 + diff(wall_z) .^ 2)) / length(dist)
-                end
-
-                #search points in rectangle between two points
-                #! format: off
-                add_r = wall_r[wall_r.>(minimum([Rwall[ind],Rwall[ind+1]])-dr ).&& wall_r .< (maximum([Rwall[ind],Rwall[ind+1]])+dr ).&&
-                               wall_z.>(minimum([Zwall[ind],Zwall[ind+1]])-dz) .&& wall_z .< (maximum([Zwall[ind],Zwall[ind+1]])+dz )  ]
-                add_z = wall_z[wall_r.>(minimum([Rwall[ind],Rwall[ind+1]])-dr) .&& wall_r .< (maximum([Rwall[ind],Rwall[ind+1]])+dr ).&&
-                               wall_z.>(minimum([Zwall[ind],Zwall[ind+1]])-dz) .&& wall_z .< (maximum([Zwall[ind],Zwall[ind+1]])+dz)   ]
-                #! format: on
-                Rwall = append!(Rwall[1:ind], add_r, Rwall[ind+1:end])
-                Zwall = append!(Zwall[1:ind], add_z, Zwall[ind+1:end])
-                Qwall = append!(Qwall[1:ind], add_r .* 0.0, Qwall[ind+1:end])
-                Qpara = append!(Qpara[1:ind], add_r .* 0.0, Qpara[ind+1:end])
-            end
-            if length(Rwall) == save_length
-                # I am not finding anymore points to add
-                break
-            end
-            dist = (diff(Rwall)) .^ 2 + (diff(Zwall)) .^ 2
-            avg = sum(dist) / length(dist) # mean distance between adjacent points in mesh
-            std = sqrt(sum((dist .- avg) .^ 2) / (length(dist) - 1)) #standard deviation of distance adjacent points in mesh
-            counter += 1
-            save_length = length(Rwall)
-        end
-        # add OMP point
-        # NOTE: this treatment of the OMP_wall point must be revised (physics missing)
-        if (r_wall_midplane, ZA) in (Rwall, Zwall)
-        else
-            Rwall = vcat(r_wall_midplane, Rwall)
-            Zwall = vcat(ZA, Zwall)
-            Qwall = vcat(0.0, Qwall) # this must be modified according to physics
-            Qpara = vcat(q_interp(r_wall_midplane), Qpara)
-        end
-    end
-
-    # Compute the heat flux due to the core radiation - Qrad - W/m2
-    # Make sure (Rwall,Zwall) is closed
-    if !((Rwall[1], Zwall[1]) == (Rwall[end], Zwall[end]))
-        # Mesh not closed!
-        push!(Rwall, Rwall[1])
-        push!(Zwall, Zwall[1])
-        push!(Qwall, Qwall[1])
-        push!(Qpara, Qpara[1])
-    end
-
+        
     return (Qwall=Qwall, Qpara=Qpara)
 end
 
@@ -421,7 +161,6 @@ end
 Computes the wall mesh for the heat flux deposited on the wall. Returns:
 
 (Rwall, Zwall)        wall mesh (with intersections of SOL) -  m
-(rwall, zwall)        evenly spaced wall mesh - m
 s                     curvilinear abscissa computed from (Rwall, Zwall), clockwise starting at OMP - m
 SOL                   list of OpenFieldLines used to compute (Rwall, Zwall)
 (r,q)                 Hypothesis of power density decay at omp for definition of SOL
@@ -737,7 +476,7 @@ function mesher_heat_flux(dd::IMAS.dd;
         push!(s, s[end] + sqrt((Rwall[1] - Rwall[end])^2 + (Zwall[1] - Zwall[end])^2))
     end
 
-    return (Rwall=Rwall, Zwall=Zwall, rwall=rwall, zwall=zwall, s=s, SOL=SOL, r=r, q=q)
+    return (Rwall=Rwall, Zwall=Zwall, s=s, SOL=SOL, r=r, q=q)
 end
 
 #= ============= =#
