5cc10bb8c8a882d83939d878c77e075e5ce34d02
David Weisberg <weisbergd@fusion.gat.com>
2026-09-14 10:14:31 -0700

    Added radiation_types to radiation_losses function (#315)

--- src/physics/radiation.jl
+++ src/physics/radiation.jl
@@ -7,10 +7,10 @@ Evaluate total plasma radiation losses [W] due to bremsstrahlung, synchrotron, a
 
 These are energy losses, so they have a negative sign.
 """
-function radiation_losses(sources::IMAS.core_sources; time0::Float64=global_time(sources))
+function radiation_losses(sources::IMAS.core_sources; time0::Float64=global_time(sources), radiation_types::Tuple=(:bremsstrahlung, :synchrotron_radiation, :line_radiation, :radiation, :cyclotron_radiation, :cyclotron_synchrotron_radiation, :impurity_radiation))
     n2i = name_2_index(sources.source)
     radiation_indices =
-        [n2i[name] for name in (:bremsstrahlung, :synchrotron_radiation, :line_radiation, :radiation, :cyclotron_radiation, :cyclotron_synchrotron_radiation, :impurity_radiation)]
+        [n2i[name] for name in radiation_types]
     radiation_energy = 0.0
     for source in sources.source
         if source.identifier.index ∈ radiation_indices
